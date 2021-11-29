accb0242dccc92f1b29937408c1c5c4cd00ec233
Dionne <mylife33@hotmail.com>
2021-11-29 14:40:52 -0600

    mvp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,89 @@
-import React from 'react';
+import React from "react";
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
+import "./components/Todo.css";
+
+const stuffToDo = [
+  {
+    task: "Sleep",
+    id: 1,
+    completed: false,
+  },
+  {
+    task: "Read",
+    id: 12,
+    done: false,
+  },
+  {
+    task: "Shower",
+    id: 13,
+    done: false,
+  },
+  {
+    task: "Run",
+    id: 15,
+    done: false,
+  },
+  {
+    task: "Yoga",
+    id: 2,
+    done: false,
+  },
+];
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      stuffToDo: stuffToDo,
+      anotherStateProp: "",
+    };
+  }
+
+  toggleItem = (id) => {
+    this.setState({
+      stuffToDo: this.state.stuffToDo.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            done: !item.done,
+          };
+        } else {
+          return item;
+        }
+      }),
+    });
+  };
+
+  clearDone = () => {
+    this.setState({
+      stuffToDo: this.state.stuffToDo.filter((item) => !item.done),
+    });
+  };
+
+  addItem = (itemName) => {
+    const newItem = {
+      task: itemName,
+      id: new Date(),
+      done: false,
+    };
+    this.setState({
+      stuffToDo: [...this.state.stuffToDo, newItem],
+    });
+  };
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+        <div className="header">
+          <h1>Welcome to Your Ultimate To Do List!</h1>
+          <TodoForm addItem={this.addItem} />
+        </div>
+        <TodoList
+          toggleItem={this.toggleItem}
+          clearDone={this.clearDone}
+          stuffToDo={this.state.stuffToDo}
+        />
       </div>
     );
   }
